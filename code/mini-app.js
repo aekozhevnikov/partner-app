0fe8f98230752b309cde1508c8b6fad4f6ac6b24
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-10-13 19:58:32 +0500

    update code/mini-app.js

--- code/mini-app.js
+++ code/mini-app.js
@@ -2,18 +2,40 @@ const tg = window.Telegram.WebApp;
 const channel = 'https://t.me/kupi_salon';
 const { user: { username, id }, start_param } = tg.initDataUnsafe;
 
-tg.expand();
-tg.BackButton.hide();
-
-// Проверка подписки на канал при старте веб-приложения
-window.addEventListener('load', function () {
-    const { is_subscribed, is_authorized } = window.location.href = `/check?partner=${start_param}&user_id=${id}`;
-    tg.showAlert({ is_subscribed, is_authorized });
-});
-
 const subscribe = document.getElementById("subscribe-button");
 const auth = document.getElementById("auth-button");
 const calculate = document.getElementById("calculate-button");
+
+tg.expand();
+tg.BackButton.hide();
+
+const checkout = {
+    as: () => { [subscribe, auth].forEach(s => s.style.display = 'none'); },
+    a: () => { [auth, calculate].forEach(s => s.style.display = 'none'); },
+    s: () => { [subscribe, calculate].forEach(s => s.style.display = 'none'); }
+};
+
+// Проверка подписки на канал при старте веб-приложения
+window.addEventListener('load', () => {
+    const { is_subscribed, is_authorized } = window.location.href = `/check?partner=${start_param}&user_id=${id}`;
+
+    const checks = {
+        a: is_authorized && !is_subscribed,
+        as: !is_authorized && !is_subscribed,
+        s: !is_authorized && is_subscribed
+    };
+
+    for (const key in checks) {
+        if (checks[key]) {
+            if (checkout[key]) {
+                checkout[key]();
+            }
+            window.location.href = '/';
+            break;
+        }
+    }
+
+});
 
 subscribe.addEventListener('click', function () {
     tg.openTelegramLink(channel);
