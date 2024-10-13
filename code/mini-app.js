48b824e5493fb9e0ad2e9479d0fa66e32abd0d30
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-10-13 15:52:19 +0500

    update code/mini-app.js

--- code/mini-app.js
+++ code/mini-app.js
@@ -1,30 +1,14 @@
-import dotenv from 'dotenv';
-
 const tg = window.Telegram.WebApp;
 const channel = 'https://t.me/kupi_salon';
 const { user: { username, id }, start_param } = tg.initDataUnsafe;
-const botToken = process.env.bot_token;
-const channel_name = process.env.kupisalonID;
 
-dotenv.config();
 tg.expand();
 tg.BackButton.hide();
 
 // Проверка подписки на канал при старте веб-приложения
 window.addEventListener('load', function () {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', `https://api.telegram.org/bot${botToken}/getChatMember?chat_id=${channel_name}&user_id=${id}`, true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var response = JSON.parse(xhr.responseText);
-            if (response.ok && response.result.status !== 'left') {
-                tg.showAlert('Подписан');
-            } else {
-                tg.showAlert('Не подписан');
-            }
-        }
-    };
-    xhr.send();
+    const { is_subscribed, is_authorized } = window.location.href = `/check`;
+    tg.showAlert({ is_subscribed, is_authorized });
 });
 
 const subscribe = document.getElementById("subscribe-button");
