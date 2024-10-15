c008ef2132923101c43de078da48c39ffc0ce075
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-10-15 15:40:51 +0500

    update

--- functions/validate.py
+++ functions/validate.py
@@ -8,4 +8,5 @@ async def HMAC_SHA256(key, secret):
 async def getCheckString(data):
     items = [(k, v) for k, v in data.items() if k != "hash"]
     items.sort(key=lambda x: x[0])  # Сортировка по ключам
-    return "n".join([f"{k}={v}" for k, v in items])
+    return items
+# "n".join([f"{k}={v}" for k, v in items])
