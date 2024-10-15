4a2b4a77f3b44f82738ea1c6c08a279cfd2498bc
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-10-15 17:38:34 +0500

    update

--- code/mini-app.js
+++ code/mini-app.js
@@ -17,38 +17,46 @@ const checkout = {
     s: () => { [subscribe, calculate].forEach(s => s.style.display = 'none'); }
 };
 
+let isCheckPerformed = false; // Флаг, чтобы отслеживать, была ли проверка выполнена
+
 const checkSubscriptionAndAuthorization = async () => {
-    try {
-        const response = await fetch(`/check?partner=${start_param}&user_id=${id}`);
-        const { is_subscribed, is_authorized } = await response.json();
+    if (!isCheckPerformed) { // Проверка, чтобы выполнить только один раз
+        try {
+            const response = await fetch(`/check?partner=${start_param}&user_id=${id}`);
+            const { is_subscribed, is_authorized } = await response.json();
 
-        const checks = {
-            a: is_authorized && !is_subscribed,
-            as: !is_authorized && !is_subscribed,
-            s: !is_authorized && is_subscribed
-        };
+            const checks = {
+                a: is_authorized && !is_subscribed,
+                as: !is_authorized && !is_subscribed,
+                s: !is_authorized && is_subscribed
+            };
 
-        for (const key in checks) {
-            if (checks[key]) {
-                if (checkout[key]) {
-                    checkout[key]();
+            for (const key in checks) {
+                if (checks[key]) {
+                    if (checkout[key]) {
+                        checkout[key]();
+                    }
+                    window.location.href = '/';
+                    break;
                 }
-                window.location.href = '/';
-                break;
             }
+        } catch (error) {
+            console.error('Error:', error);
         }
-    } catch (error) {
-        console.error('Error:', error);
+
+        isCheckPerformed = true; // Установка флага в true, чтобы указать, что проверка выполнена
     }
-}
+};
 
-window.addEventListener("load", async function () {
-    console.log(tg.initData);
-    const data = await fetch(`/validate-init?${tg.initData}`).then(res => res.json());
-    console.log(data);
+window.addEventListener('load', async function () {
+    if (!isCheckPerformed) {
+        console.log(tg.initData);
+        const data = await fetch(`/validate-init?${tg.initData}`).then(res => res.json());
+        console.log(data);
+
+        checkSubscriptionAndAuthorization(); // Вызов функции проверки после загрузки данных
+    }
 });
-
-window.addEventListener('load', checkSubscriptionAndAuthorization);
 
 subscribe.addEventListener('click', function () {
     tg.openTelegramLink(channel);
