2c37cba4172a7d619b83fee816af62d2bd5a5acf
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-10-15 22:22:46 +0500

    update

--- main.py
+++ main.py
@@ -28,24 +28,17 @@ logger.addHandler(file_handler)
 # Конфигурирование маршрутов
 route_conf = configure_routes(app, dp, bot)
 
-async def start_app():
-    runner = web.AppRunner(app)
-    await runner.setup()
-    site = web.TCPSite(runner, '0.0.0.0', 5000)
-    await site.start()
-
-async def stop_app():
-    app.shutdown()
-
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.create_task(route_conf.on_startup(dp))  # Вызов установки Webhook асинхронно
-    loop.create_task(dp.start_polling())  # Запуск Polling асинхронно
-    loop.create_task(start_app())  # Запуск aiohttp-приложения
+    async def main():
+        runner = web.AppRunner(app)
+        await runner.setup()
+        site = web.TCPSite(runner, '0.0.0.0', 5000)
+        await site.start()
+
+        await route_conf.on_startup(dp)
+        await dp.start_polling()
+
     try:
-        loop.run_forever()
+        asyncio.run(main())
     except KeyboardInterrupt:
-        pass  # Остановка при нажатии Ctrl+C
-    finally:
-        loop.run_until_complete(stop_app())
-        loop.close()
+        pass
