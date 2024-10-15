143cabc2959f83c0717cf6e52d14f190e17e8d62
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-10-15 12:25:52 +0500

    update

--- routes.py
+++ routes.py
@@ -100,9 +100,10 @@ def configure_routes(app, dp, bot):
             loop = asyncio.get_event_loop()
             
             try:
-                values_list = list(request.args.values())
-                logger.debug(values_list)
-                success = asyncio.run(save(arr=values_list))
+                values_dict = request.args.to_dict()  # Преобразование параметров запроса в словарь
+                logger.debug(values_dict)
+                
+                success = asyncio.run(save(arr=list(values_dict.values())))
                 
                 loop.close()
                 return jsonify(success=success)
