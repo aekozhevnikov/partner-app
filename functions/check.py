0b99574845ed31d39e57f814c2d585f1f9ac6ba3
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-10-16 10:55:41 +0500

    update

--- functions/check.py
+++ functions/check.py
@@ -18,7 +18,7 @@ logger.addHandler(handler)
 async def subscription(bot: Bot) -> bool:
     try:
         
-        member = await bot.get_chat_member(chat_id=KUPISALONID, user_id=bot.id)
+        member = bot.get_chat_member(chat_id=KUPISALONID, user_id=bot.id)
         return member.is_chat_member()
     except Exception as e:
         logger.error(f"An error occurred in check_subscription: {e}")
