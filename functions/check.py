3b60cc0f00129161155d18af5d23ec3d8a61f801
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-10-17 23:54:52 +0500

    update

--- functions/check.py
+++ functions/check.py
@@ -20,7 +20,7 @@ async def subscription(bot: Bot) -> bool:
         
         loop = asyncio.get_event_loop()
         
-        member = await loop.run_in_executor(None, bot.get_chat_member(chat_id=KUPISALONID, user_id=bot.id))
+        member = await bot.get_chat_member(chat_id=KUPISALONID, user_id=bot.id)
         return member.status in ('administrator', 'creator')
     except Exception as e:
         logger.error(f"An error occurred in check_subscription: {e}")
